377a9d3ee28de8fd066f02b3ce36ac2514c12631
longtao <1026637705@qq.com>
2020-05-18 18:01:04 +0800

    外星人侵入

--- projects/alien_invasion/alien_invasion.py
+++ projects/alien_invasion/alien_invasion.py
@@ -3,17 +3,30 @@
 
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
+
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+
+    setting = Settings()
+
+    screen = pygame.display.set_mode(
+        (setting.screen_width, setting.screent_height))
     pygame.display.set_caption('Alien Invasion')
+
+    ship = Ship(screen)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        screen.fill(setting.bg_color)
+        ship.blitme()
+
         pygame.display.flip()
+
 
 run_game()
---
+++ projects/alien_invasion/settings.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python3
+# coding=utf-8
+
+class Settings():
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screent_height = 800
+        self.bg_color = (230, 230, 230)
+    
